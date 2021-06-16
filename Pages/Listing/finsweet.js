bb215d2851c37743a402970c18a893e6e574cc66
Santiago Teran <santerans@gmail.com>
2021-06-16 17:35:46 -0500

    Inicial FINSWEET

---
+++ Pages/Listing/finsweet.js
@@ -0,0 +1,63 @@
+(function() {
+	var projectsGrid = new FsLibrary('.listing-prop-collection-list');
+		
+  var myFilters = [
+    {filterWrapper: ".tipo-filters", filterType: "multi", filterByClass:".listing-type",},
+    {filterWrapper: ".area-filters", filterType: "exclusive", filterByClass:".area-const",filterRange: true},
+    {filterWrapper: ".precio-filters", filterType: "exclusive", filterByClass:".property-cost",filterRange: true},
+    {filterWrapper: ".banos-filters", filterType: "exclusive", filterByClass:".banos",filterRange: true},
+    {filterWrapper: ".hab-filters", filterType: "exclusive", filterByClass:".hab",filterRange: true},
+    {filterWrapper: ".badge-filters", filterType: "exclusive", filterByClass:".badge",},
+    {filterWrapper: ".place-filters", filterType: "exclusive", filterByClass:".listing-address",},
+    {filterWrapper: ".lupa-wrap", filterType: "exclusive", filterByClass:".listing-address",},
+  ]
+
+	projectsGrid.filter({
+		filterArray: myFilters,
+    activeClass: 'selected',
+    filterReset:'.limpiar-filtro',
+		animation: {
+			enable: true,
+			duration: 200,
+			easing: 'ease-out',
+			effects: 'fade translate(0px,20px)'
+			}
+  })
+	
+  projectsGrid.sort({
+    sortTrigger:".sort-button-desc",
+    sortReverse: true,
+    activeClass:"sort-active",
+    animation: {
+      duration: 0
+    }
+  })
+	
+  projectsGrid.sort({
+    sortTrigger:".sort-button-asc",
+    sortReverse: false,
+    activeClass:"sort-active",
+    animation: {
+      duration: 0
+    }
+  })
+  
+  projectsGrid.loadmore({
+		button: ".load-more-button",
+    resetIx: true,
+    loadAll: true,
+    paginate: {
+			enable: true,
+			itemsPerPage: 20,
+			insertPagination: '.pagination-container',
+			bgColor: '#FFFFFF',
+			bgColorActive: '#240c2e',
+			textColor: '#240c2e',
+      textColorActive: '#FFFFFF',
+			borderColor: '#3D315B'
+    },
+		animation: {
+			enable: false
+		}
+  })
+  })();
