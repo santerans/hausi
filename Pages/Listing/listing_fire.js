e44938a4c5adabfb97220a7d4bf7d761109d7c35
Santiago Teran <santerans@gmail.com>
2021-09-21 12:43:26 -0500

    firebase 4

--- Pages/Listing/listing_fire.js
+++ Pages/Listing/listing_fire.js
@@ -10,13 +10,14 @@ const getTask = (id) => db.collection("listing").doc(id).get();
 const updateTask = (id, updatedTask) => db.collection('listing').doc(id).update(updatedTask);
 
 window.addEventListener("DOMContentLoaded", async (e) => {
-    var listingCard = $('#listing-card').html();
+    var listingCard = document.querySelector('listing-card').innerHTML;
 
     onGetTasks((querySnapshot) => {
         querySnapshot.forEach((doc) => {
         const prop = doc.data();
-        var place_div = $("<div>", {"class": "place"});
-        place_div.append(listingCard);
+        place_div = document.createElement("DIV");
+        place_div.setAttribute("class", "place");
+        place_div.appendChild(listingCard);
         place_div.find('.property-price').text(prop.precio);
         });
     });
