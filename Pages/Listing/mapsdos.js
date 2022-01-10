174851c117ba6f05ce7769084d34b1bccdbef209
santerans <66583413+santerans@users.noreply.github.com>
2022-01-10 09:57:43 -0500

    Update mapsdos.js

--- Pages/Listing/mapsdos.js
+++ Pages/Listing/mapsdos.js
@@ -48,13 +48,6 @@
         var priceVenta = kFormatter(costoVenta);
         var costoRenta = elem.querySelector('#costo-renta').innerText;
         var priceRenta = kFormatter(costoRenta);
-        var infoText = elem.querySelector('.property-listing-bottom-wrap').innerHTML;
-        var slideuno = elem.querySelector('.slide1').style.backgroundImage;
-        var slidedos = elem.querySelector('.slide2').style.backgroundImage;
-        var slidetres = elem.querySelector('.slide3').style.backgroundImage;
-        var slidecuarto = elem.querySelector('.slide4').style.backgroundImage;
-        var href = elem.querySelector('.property-card-link').href;
-        var display = elem.style.display;
         var ubicacion = elem.querySelector('.ubicacion').innerText;
         var ubicacion_array = ubicacion.split(', ');
         var lat = ubicacion_array[0];
@@ -65,12 +58,12 @@
         var pLong = parseFloat(long);
 
         if (window.getComputedStyle(elem).display !== "none" && priceVenta !== "$0") {
-          place.push(title, infoText, pLat, pLong, priceVenta, anchorID, display, slideuno, slidedos, slidetres, slidecuarto, href,); // Adding required info about each place to the array of places
+          place.push(title, pLat, pLong, priceVenta, anchorID); // Adding required info about each place to the array of places
           locations.push(place);
         }
 
         if (window.getComputedStyle(elem).display !== "none" && priceRenta !== "$0") {
-          place.push(title, infoText, pLat, pLong, priceRenta, anchorID, display, slideuno, slidedos, slidetres, slidecuarto, href,); // Adding required info about each place to the array of places
+          place.push(title, pLat, pLong, priceVenta, anchorID); // Adding required info about each place to the array of places
           locations.push(place);
         }
       });
@@ -110,7 +103,7 @@
           anchor: new google.maps.Point(0, 0),
         };    
       
-      function createMarker(latlng, price, anchorid, svg, slideuno, slidedos,slidetres, slidecuarto, href, html, markerClass) {
+      function createMarker(latlng, price, anchorid, svg, markerClass) {
 
         var marker = new markerWithLabel.MarkerWithLabel({
           position: latlng,
@@ -129,13 +122,6 @@
           if(window.innerWidth <= 478){
                document.getElementById('list-button').style.display = "none";
                document.getElementById('house').style.display = "block";
-               document.getElementById('house-detail').innerHTML = html;
-               document.getElementById('property-card-link').href = href;
-               document.getElementById('slider1').style.backgroundImage = slideuno;
-               document.getElementById('slider2').style.backgroundImage = slidedos;
-               document.getElementById('slider3').style.backgroundImage = slidetres;
-               document.getElementById('slider4').style.backgroundImage = slidecuarto;
-               document.getElementById('slider5').style.backgroundImage = slidetres;
                $('.price-label-selected').addClass('price-label');
                $('.price-label-selected').removeClass('price-label-selected');
                marker.set("labelClass", "price-label-selected")
@@ -166,7 +152,7 @@
       let gmarkers = [];
       for (var i = 0; i < locations.length; i++) {
         gmarkers[locations[i][0]] =
-          createMarker(new google.maps.LatLng(locations[i][2], locations[i][3]),locations[i][4],locations[i][5],svgMarker,locations[i][7],locations[i][8],locations[i][9],locations[i][10],locations[i][11], // passing lat and long
+          createMarker(new google.maps.LatLng(locations[i][1], locations[i][2]),locations[i][3],locations[i][4],svgMarker, // passing lat and long
                                   locations[i][1],"price-label"); // passing Info-window information
       }
 
